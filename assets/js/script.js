e80d47f3058f68e1580caaca4efba1453504a6ae
Carlos <148721970+ceosbeifdee@users.noreply.github.com>
2025-01-18 19:44:36 -0300

    aaa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,41 @@
-const anos = document.querySelector("#anos")
-const meses = document.querySelector("#meses")
-const semanas = document.querySelector("#semanas")
-const dias = document.querySelector("#dias")
-const horas = document.querySelector("#horas")
-const minutos = document.querySelector("#minutos")
-const segundos = document.querySelector("#segundos")
+// Date.UTC()
+// const anoAtual = new Date().getFullYear()
+// const mesAtual = new Date().getMonth()
+// const diasNoMes = new Date(anoAtual, mesAtual + 1, 0).getDate()
 
-const relogio = setInterval(function time() {
+// const anos = document.querySelector("#anos")
+// const meses = document.querySelector("#meses")
+// const semanas = document.querySelector("#semanas")
+// const dias = document.querySelector("#dias")
+// const horas = document.querySelector("#horas")
+// const minutos = document.querySelector("#minutos")
+// const segundos = document.querySelector("#segundos")
 
-    let encontro = new Date("2024-11-16T23:38:00-03:00")
-    let hoje = new Date()
-    let tempoPassado = hoje - encontro
+// const relogio = setInterval(function time() {
+//     const encontro = new Date("2024-11-16T23:38:00-03:00")
+//     const hoje = new Date()
 
-    let anosPassados = Math.floor(tempoPassado / (1000 * 60 * 60 * 24 * 365))
-     let mesesPassados = Math.floor((tempoPassado % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30))
-    let semanasPassados = Math.floor((tempoPassado % (1000 * 60 * 60 * 24 * 7)) / (1000 * 60 * 60 * 24 * 7))
-    let diasPassados = Math.floor((tempoPassado % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24))
-    let horasPassados = Math.floor((tempoPassado % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))
-    let minutosPassados = Math.floor((tempoPassado % (1000 * 60 * 60) / (1000 * 60)))
-    let segundosPassados = Math.floor((tempoPassado % (1000 * 60) / 1000))
+//     const diff = new Date(hoje - encontro)
+    
+//         const anosPassados = diff.getUTCFullYear() - 1970
+    
+//         const mesesPassados = diff.getUTCMonth()
+    
+//         const diasPassados = 
 
-    anos.textContent = anosPassados < 10 ? `0${anosPassados}` : anosPassados
-    meses.textContent = mesesPassados < 10 ? `0${mesesPassados}` : mesesPassados
-    semanas.textContent = semanasPassados < 10 ? `0${semanasPassados}` : semanasPassados
-    dias.textContent = diasPassados < 10 ? `0${diasPassados}` : diasPassados
-    horas.textContent = horasPassados < 10 ? `0${horasPassados}` : horasPassados
-    minutos.textContent = minutosPassados < 10 ? `0${minutosPassados}` : minutosPassados
-    segundos.textContent = segundosPassados < 10 ? `0${segundosPassados}` : segundosPassados
+//         const semanasPassadas = Math.floor(diasPassados / 7)
+    
+//         const horasPassadas = diff.getUTCHours()
 
+//         const minutosPassados = diff.getUTCMinutes()
 
-})
+//         const segundosPassados = diff.getUTCSeconds()
+
+//     anos.textContent = anosPassados < 10 ? `0${anosPassados}` : anosPassados
+//     meses.textContent = mesesPassados < 10 ? `0${mesesPassados}` : mesesPassados
+//     semanas.textContent = semanasPassadas < 10 ? `0${semanasPassadas}` : semanasPassadas
+//     dias.textContent = diasPassados < 10 ? `0${diasPassados}` : diasPassados
+//     horas.textContent = horasPassadas < 10 ? `0${horasPassadas}` : horasPassadas
+//     minutos.textContent = minutosPassados < 10 ? `0${minutosPassados}` : minutosPassados
+//     segundos.textContent = segundosPassados < 10 ? `0${segundosPassados}` : segundosPassados
+// })
