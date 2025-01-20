a0ec81a40ee15856d4e8c86325c0d5c3230c1ce6
Carlos <148721970+ceosbeifdee@users.noreply.github.com>
2025-01-20 09:51:24 -0300

    ijh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,38 @@
+const anos = document.querySelector("#anos")
+const meses = document.querySelector("#meses")
+const dias = document.querySelector("#dias")
+const horas = document.querySelector("#horas")
+const minutos = document.querySelector("#minutos")
+const segundos = document.querySelector("#segundos")
+
+const relogio = setInterval(function time() {
+    const encontro = new Date("2024-11-16T23:38:00")
+    const hoje = new Date()
+    const tempoPassado = new Date(hoje - encontro)
+    const mesAnterior = new Date(hoje, 0)
+    const anoAtual = hoje.getFullYear()
+    const diasNoMes = new Date(anoAtual, mesAnterior, 0)
+
+    const segundosPassados = Math.floor(tempoPassado / 1000) % 60
+    const minutosPassados = Math.floor(tempoPassado / (1000 * 60)) % 60
+    const horasPassadas = Math.floor(tempoPassado / (1000 * 60 * 60)) % 24
+    const diasPassados = Math.floor(tempoPassado / (1000 * 60 * 60 * 24)) % diasNoMes
+    const mesesPassados = tempoPassado.getMonth()
+    const anosPassados = tempoPassado.getFullYear() - 1970
+
+    anos.textContent = anosPassados < 10 ? `0${anosPassados}` : anosPassados
+    meses.textContent = mesesPassados < 10 ? `0${mesesPassados}` : mesesPassados
+    dias.textContent = diasPassados < 10 ? `0${diasPassados}` : diasPassados
+    horas.textContent = horasPassadas < 10 ? `0${horasPassadas}` : horasPassadas
+    minutos.textContent = minutosPassados < 10 ? `0${minutosPassados}` : minutosPassados
+    segundos.textContent = segundosPassados < 10 ? `0${segundosPassados}` : segundosPassados
+})
+
+// semanas.textContent = semanasPassadas < 10 ? `0${semanasPassadas}` : semanasPassadas
+
 // Date.UTC()
 // const anoAtual = new Date().getFullYear()
 // const mesAtual = new Date().getMonth()
-// const diasNoMes = new Date(anoAtual, mesAtual + 1, 0).getDate()
-
-// const anos = document.querySelector("#anos")
-// const meses = document.querySelector("#meses")
-// const semanas = document.querySelector("#semanas")
-// const dias = document.querySelector("#dias")
-// const horas = document.querySelector("#horas")
-// const minutos = document.querySelector("#minutos")
-// const segundos = document.querySelector("#segundos")
 
 // const relogio = setInterval(function time() {
 //     const encontro = new Date("2024-11-16T23:38:00-03:00")
@@ -31,11 +54,5 @@
 
 //         const segundosPassados = diff.getUTCSeconds()
 
-//     anos.textContent = anosPassados < 10 ? `0${anosPassados}` : anosPassados
-//     meses.textContent = mesesPassados < 10 ? `0${mesesPassados}` : mesesPassados
-//     semanas.textContent = semanasPassadas < 10 ? `0${semanasPassadas}` : semanasPassadas
-//     dias.textContent = diasPassados < 10 ? `0${diasPassados}` : diasPassados
-//     horas.textContent = horasPassadas < 10 ? `0${horasPassadas}` : horasPassadas
-//     minutos.textContent = minutosPassados < 10 ? `0${minutosPassados}` : minutosPassados
-//     segundos.textContent = segundosPassados < 10 ? `0${segundosPassados}` : segundosPassados
+
 // })
